92d9d8004c5a4c6129d435453ffb910425e705cc
Olena Klementieva <foksysmile@gmail.com>
2023-11-20 15:10:47 +0200

    init

--- src/script/test.js
+++ src/script/test.js
@@ -1,1 +1,19 @@
-console.log('script')
+// console.log('script')
+
+// export const message = 'script'
+
+// export const message2 = () => {}
+
+// export function message3() {}
+
+// або: 
+
+const message = 'script'
+
+const message2 = () => {}
+
+function message3() {}
+
+export {message, message2, message3}
+
+export default () => {}
